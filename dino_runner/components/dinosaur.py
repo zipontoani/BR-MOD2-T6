4fb99fcf25edd46d875f23a1e7c084799bee7898
Cleiseane Gibin <cleisegibinane@gmail.com>
2022-10-25 14:00:22 -0300

    Classe 1 - Dinosaur (Duck)
    Implementação da lógica do método duck() com ajuste no Y para os assets DUCKING ficarem alinhados com os de RUNNING;
    Implementação dos inputs para chamar o Duck (Teclas Down, RAlt e S*);
    
    *S parece não estar funcionando :S

--- dino_runner/components/dinosaur.py
+++ dino_runner/components/dinosaur.py
@@ -1,6 +1,6 @@
 from pickle import TRUE
 import pygame
-from dino_runner.utils.constants import RUNNING, X_POS, Y_POS, JUMPING, JUMP_VEL
+from dino_runner.utils.constants import DUCKING, RUNNING, X_POS, Y_POS, JUMPING, JUMP_VEL
 
 class Dinosaur:
     def __init__(self):
@@ -12,18 +12,28 @@ class Dinosaur:
         self.dino_run = True
         self.dino_jump = False
         self.jump_vel = JUMP_VEL
+        self.dino_duck = False
 
     def update(self, user_input):
         if self.dino_run:
             self.run()
         elif self.dino_jump:
             self.jump()
+        elif self.dino_duck:
+            self.duck()
 
         if user_input[pygame.K_UP] or user_input[pygame.K_SPACE] or user_input[pygame.K_w] and not self.dino_jump:
             self.dino_jump = True
             self.dino_run = False
         elif not self.dino_jump:
             self.dino_jump = False
+            self.dino_run = True  
+
+        if user_input[pygame.K_DOWN] or user_input[pygame.K_RALT] or user_input[pygame.K_s] and not self.dino_duck:
+            self.dino_duck = True
+            self.dino_run = False
+        elif not self.dino_duck:
+            self.dino_duck = False
             self.dino_run = True  
             
         if self.step_index >= 10:
@@ -35,9 +45,6 @@ class Dinosaur:
         self.dino_rect.x = X_POS
         self.dino_rect.y = Y_POS
         self.step_index += 1
-
-        
-
 
     def draw(self, screen):
         screen.blit(self.image, (self.dino_rect.x, self.dino_rect.y))
@@ -55,4 +62,9 @@ class Dinosaur:
 
 
     def duck(self):
-        pass
+        self.image = DUCKING[0] if self.step_index < 5 else DUCKING[1]
+        self.dino_rect = self.image.get_rect()
+        self.dino_rect.x = X_POS 
+        self.dino_rect.y = Y_POS + 30.5 
+        self.step_index += 1
+
